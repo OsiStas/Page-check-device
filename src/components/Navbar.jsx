2aaea05a8daa34f159f22d6ed19b175607aaab91
OsiStas <osigit17@gmail.com>
2023-05-13 09:01:01 +0300

    Дороблена авторизація

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,12 @@ import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
 const NavBar = observer(() => {
   const { user } = useContext(Context);
   const navigate = useNavigate();
+
+  const logOut = () => {
+    user.setUser({});
+    user.setIsAuth(false);
+  };
+
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
@@ -17,13 +23,16 @@ const NavBar = observer(() => {
         </Link>
         {user.isAuth ? (
           <Nav className="ms-auto" style={{ color: 'white' }}>
-            <Button variant={'outline-light'} onClick={() => navigate(ADMIN_ROUTE)}>
+            <Button
+              variant={'outline-light'}
+              onClick={() => navigate(ADMIN_ROUTE)}
+            >
               Админ панель
             </Button>
             <Button
               variant={'outline-light'}
               className="ms-2"
-              onClick={() => navigate(LOGIN_ROUTE)}
+              onClick={() => logOut() }
             >
               Вийти
             </Button>
@@ -33,7 +42,7 @@ const NavBar = observer(() => {
             <Button
               variant={'outline-light'}
               className="ms-2"
-              onClick={() => user.setIsAuth(true)}
+              onClick={() => navigate(LOGIN_ROUTE)}
             >
               Авторизація
             </Button>
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,28 +1,33 @@
 import React, { useContext, useState } from 'react';
 import { Container, Form, Card, Button, Row } from 'react-bootstrap';
-import { Link, useLocation } from 'react-router-dom';
-import { REGISTRATION_ROUTE, LOGIN_ROUTE } from '../utils/consts';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { REGISTRATION_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
 import { login, registration } from '../http/userApi';
 import { observer } from 'mobx-react-lite';
-import {Context} from '../index'
-
+import { Context } from '../index';
 
 const Auth = observer(() => {
-  const {user} = useContext(Context)
+  const { user } = useContext(Context);
   const location = useLocation();
   const isLogin = location.pathname === LOGIN_ROUTE;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate()
 
   const click = async () => {
-    let data;
+    try {
+      let data;
       if (isLogin) {
-        data = await login(email,password);
+        data = await login(email, password);
       } else {
         data = await registration(email, password);
       }
-      user.setUser(user)
-      user.setIsAuth(true)
+      user.setUser(user);
+      user.setIsAuth(true);
+      navigate(SHOP_ROUTE)
+    } catch (e) {
+      alert(e.response.data.message);
+    }
   };
 
   return (
